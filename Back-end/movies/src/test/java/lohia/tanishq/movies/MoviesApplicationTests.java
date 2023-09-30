02f2f99f2014ce9f9b20a498812afd27f74c62a4
tanishq2507 <smartlohia@gmail.com>
2023-10-01 02:11:59 +0530

    added back-end

---
+++ Back-end/movies/src/test/java/lohia/tanishq/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package lohia.tanishq.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
